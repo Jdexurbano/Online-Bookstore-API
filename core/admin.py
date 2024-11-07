a17f722bd1afb03c59748b11df6451311b7fcc2f
Jdexurbano <dexterurbano25@gmail.com>
2024-11-07 18:50:34 +0800

    add custom user model

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + ((None,{'fields':('address','contact_number','role')}),)
+
+    list_display = ('id','username','address','contact_number','role')
 
 # Register your models here.
+admin.site.register(CustomUser,CustomUserAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+
+    #choices for role
+    ROLE_CHOICES = [
+        ('user','User'),
+        ('admin','Admin'),
+    ]
+
+    address = models.CharField(max_length = 100)
+    contact_number = models.CharField(max_length = 15)
+    role = models.CharField(max_length = 10, choices = ROLE_CHOICES, default = 'user')
+
+    def __str__(self):
+        return str(f"{self.first_name} {self.last_name}")
+    
