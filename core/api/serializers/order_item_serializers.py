eff0802a75629e12570cc637f1008a76bc613b1a
Jdexurbano <dexterurbano25@gmail.com>
2024-11-09 23:46:44 +0800

    add order item serializer.

---
+++ core/api/serializers/order_item_serializers.py
@@ -0,0 +1,25 @@
+from core.models import OrderItem,CustomUser,Book, Order
+from rest_framework import serializers
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = ['id','book','quantity']
+    
+    def create(self, validated_data):
+       #create an order
+        order = Order.objects.create(user = self.context['user'])
+
+        #create the order item
+        order_item = OrderItem.objects.create(
+            order = order,
+            book = validated_data['book'],
+            quantity = validated_data['quantity']
+        )
+
+        #update the quantiy of the book if someone order
+        book = validated_data['book']
+        book.quantity -= validated_data['quantity']
+        book.save()
+
+        return order_item
